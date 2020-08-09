7de08b4896339ba88747f93f5c02e7086d3cc5c5
Callum Bennett <66990734+ctoboe5@users.noreply.github.com>
2020-08-10 09:39:20 +1000

    Guess the number v1
    Might not work

---
+++ javascript/guessTheNumber.js
@@ -0,0 +1,21 @@
+function playGame() {
+    var guesses = 0;
+    var playerInput;
+    var answer = 5;
+    guesses = 0;
+    playerInput = prompt("Enter a number between 1 and 10")
+    if (playerInput > answer) {
+        alert("Your guess was too high");
+        guesses = guesses + 1;
+    } else if (playerInput < answer) {
+        alert("Your guess was too low");
+        guesses = guesses + 1;
+    } else if (playerInput = answer) {
+        guesses = guesses + 1;
+        alert("You guessed the number in " + guesses + " guesses.";
+    } else if (playerInput = null) {
+        return;
+    } else {
+        alert("Please enter a number");
+    }
+}
