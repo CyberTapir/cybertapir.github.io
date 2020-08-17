cbf4ff92b9bbf8082b25b82851f23dacaf55e008
Callum Bennett <66990734+ctoboe5@users.noreply.github.com>
2020-08-17 10:30:52 +1000

    Update guessTheNumber.js

--- javascript/guessTheNumber.js
+++ javascript/guessTheNumber.js
@@ -1,22 +1,25 @@
+button.onclick = playGame();
 function playGame() {
     var guesses = 0;
     var playerInput;
-    //At this point for testing, the answer will stay 5
     var answer = 5;
+    var hasWon = false;
+    
     guesses = 0;
-    playerInput = prompt("Enter a number between 1 and 10")
-    if (playerInput > answer) {
-        alert("Your guess was too high");
-        guesses = guesses + 1;
-    } else if (playerInput < answer) {
-        alert("Your guess was too low");
-        guesses = guesses + 1;
-    } else if (playerInput = answer) {
-        guesses = guesses + 1;
-        alert("You guessed the number in " + guesses + " guesses.";
-    } else if (playerInput = null) {
-        return;
-    } else {
-        alert("Please enter a number");
+    while (hasWon == false) {
+    playerInput = prompt("Enter a number between 1 and 10");
+        if (playerInput > answer) {
+            alert("Your guess was too high");
+            guesses = guesses + 1;
+        } else if (playerInput < answer) {
+            alert("Your guess was too low");
+            guesses = guesses + 1;
+        } else if (playerInput == answer) {
+            guesses = guesses + 1;
+            alert("You guessed the number in " + guesses + " guesses.");
+            hasWon = true;
+        } else {
+            alert("Please enter a number");
+        }
     }
 }
