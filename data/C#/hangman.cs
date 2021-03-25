a467b848cb6d50bcb1aed3b041cd4f3feb18ed24
Callum Bennett <66990734+callumbennett-dev@users.noreply.github.com>
2021-03-25 15:53:24 +1100

    Create hangman.cs

---
+++ data/C#/hangman.cs
@@ -0,0 +1,27 @@
+using System;
+namespace hangman
+{
+  class Program
+  {
+    //main method
+    //Ask for either instructions or play
+    
+    //Instructions method
+    //Output instructions
+    //Console.readline to return to main
+  
+    //gameplay method
+    //Words
+    string[] possibleWords = {"", "", ""};
+    //pick a random word
+    //save the word as a char string
+    //Write out the _s according to the length of the word
+    //Write out the lives left
+    //User guessing letter
+    //if
+      //Letter is in word
+        //Rewrite console, replacing the correct letter
+      //letter is not in word
+        //rewrite console, updating the lives left
+  }
+}
