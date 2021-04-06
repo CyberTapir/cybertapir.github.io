c959c2a0ac9a41c86ce325e70eebe606d843f260
Callum Bennett <66990734+callumbennett-dev@users.noreply.github.com>
2021-04-07 08:32:35 +1000

    added how to java

---
+++ data/howto/jar/java.java
@@ -0,0 +1,34 @@
+//Java. Extension is .jar
+
+/*to compile run 
+javac *name*.java
+
+to run, execute
+java *name*
+*/
+
+public class Person {
+    //defining the class
+    public static void main(String[] args) {
+        //main method, similar to C#
+        System.out.Println("Hello World!");
+        //Writing Hello World
+
+        char answer = 'y'; //Single character variable, must be in single quotes
+
+        int num = 5; //Whole number variable, no quotes
+
+        num++; //Adds one to num
+
+        num--; //Takes one away from num
+
+        //Variable types
+        int age = 28;
+        char grade = 'A';
+        boolean late = false;
+        byte b = 20;
+        long num1 = 1234567;
+        short no = 10;
+        float k = (float)12.5;
+        double pi = 3.14159;
+}
