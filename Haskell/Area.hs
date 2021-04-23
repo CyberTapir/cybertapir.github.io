1682c53d17ea57325422345fbc6f38251f356ca9
Callum Bennett <66990734+callumbennett-dev@users.noreply.github.com>
2021-04-23 16:25:02 +1000

    Haskell

---
+++ Haskell/Area.hs
@@ -0,0 +1,12 @@
+module Area where
+
+-- add 1 to an integer
+addOne :: Integer -> Integer
+addOne int = int + 1
+
+-- calculate area of a square with only integer lengths
+areaSquare :: Integer -> Integer
+areaSquare len = len * len
+
+-- calculate area of a rectangle with only integer lengths
+areaRectangle :: 
