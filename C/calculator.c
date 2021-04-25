21feed73cbb0f76d659b4a71809481e98e872956
Callum Bennett <66990734+callumbennett-dev@users.noreply.github.com>
2021-04-25 11:18:07 +1000

    j

--- C/calculator.c
+++ C/calculator.c
@@ -6,3 +6,31 @@
 //
 
 #include <stdio.h>
+int main(){
+    //Get input stored in a variable
+    printf("Enter your equation with a space between the operator and numbers > ");
+    string dataInput = input;
+    //store seperated floats in variables
+    inpNum1 = dataInput[0];
+    inpNum2 = dataInput[4];
+    inpOperator = dataInput[2];
+    //call the function with the inputs
+    calculate(inpNum1, inpNum2, inpOperator);
+}
+
+Int calculate(float num1; float num2; string operator){
+    //Set result to 0
+    float result = 0;
+    //if opetater is ==, calculate result
+    if (operator == "+"){
+        result = num1 + num2;
+    } else if (operator == "-"){
+        result = num1 - num2;
+    } else if (operator == "*"){
+        result = num1 * num2;
+    } else if (operator == "/"){
+        result = num1 / num2;
+    }
+    //write result
+    printf(result);
+}
---
+++ C/learnFile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+/*C Comments*/
+    printf("TestlearnFile\n");
+    return(0);
+}
