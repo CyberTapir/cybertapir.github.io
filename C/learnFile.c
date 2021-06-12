0f2cd695a412d0f038ad610a878675a458cae253
Callum Bennett <66990734+callumbennett-dev@users.noreply.github.com>
2021-06-12 14:26:57 +1000

    Offline stuff to be uploaded later

--- C/learnFile.c
+++ C/learnFile.c
@@ -1,10 +1,10 @@
-#include <stdio.h>//Main framework (i think)
-int main(){//main method to execute
+#include <stdio.h>  //Main framework (i think)
+int main(){  //main method to execute
 /*C Comments*/
     printf("TestlearnFile\n");//prints message
-    notMain();//Calling the method
-    return(0);//terminates program
-}//closing the main method
+    notMain();  //Calling the method
+    return(0);  //terminates program
+}  //closing the main method
 /*In C, methods can be called in the same way as C# */
 int notMain(){
 	//This is a C Method
---
+++ chuck/chord2.ck
@@ -0,0 +1,6 @@
+SinOsc x => SinOsc y => dac;
+220 => x.freq;
+1 => x.gain;
+220 => y.freq;
+1 => y.gain;
+2::second => now;
