51a4f0176cfe247bec7dbf68e4ce045cb024b4a9
Callum Bennett <callumbennett-dev@users.noreply.github.com>
2021-06-21 12:37:13 +1000

    k

--- node/firstModule.js
+++ node/firstModule.js
@@ -3,6 +3,6 @@ const jokes = require('give-me-a-joke');
 jokes.getRandomDadJoke( function(joke) {
     console.log(joke);
 });
-joles.getRandomCNJoke( (joke) => {
+jokes.getRandomCNJoke( (joke) => {
     console.log(joke);
 });
