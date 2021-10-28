f294037a0bd4be9c858df306be4e0e2399eb2eee
Callum Bennett <callumtbennett@gmail.com>
2021-10-28 15:23:31 +1100

    added files, and edited some HTML

--- python/counter.py
+++ python/counter.py
@@ -1,11 +1,9 @@
 start = 100
 value = 1
 result = 0
-
 for i in range(start):
 	result += value
 	value = value + 1
-	print(result)
-	if value == start:
+	if value == start + 1:
 		print(result)
 	continue
