595bac87d40437450365ba316a965bf2f3d25e0e
Callum Bennett <66990734+CyberTapir@users.noreply.github.com>
2021-11-02 12:39:26 +1100

    Updates 2/11/2021

---
+++ java/calculator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in); 
+        System.out.println("Enter your equation with spaces (5 + 3) > ");
+        String equation = sc.nextLine();
+        System.out.println(equation);
+    }
+}
