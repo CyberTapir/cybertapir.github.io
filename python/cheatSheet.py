7fe4d36fb62d50e9a1e98e2ab5926a697c7944d3
Callum Bennett <66990734+CyberTapir@users.noreply.github.com>
2021-11-14 09:56:53 +1100

    Added files

---
+++ python/cheatSheet.py
@@ -0,0 +1,16 @@
+import random
+
+#Read Input
+f = open('input.txt', 'r')
+read_data = f.read()
+	
+f.close()
+
+lines = read_data.split('\n')
+
+#Split a string
+
+str.split(" ") #Use the char(s) you want to split
+
+#random number 
+random.randrange(0, 2)
