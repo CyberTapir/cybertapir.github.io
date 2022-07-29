0242b32f66507f02a8b4ecb08db4a13f1e29f176
Callum Bennett <66990734+CyberTapir@users.noreply.github.com>
2022-07-29 10:24:55 +1000

    Create 8ball.js
    A program to replace the 8-ball

---
+++ javascript/8ball.js
@@ -0,0 +1,31 @@
+console.log('Hello!');
+
+let userQuestion = 'Are you human?';
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8); 
+var eightBall = "";
+
+if (randomNumber = 0) {
+    eightBall = 'It is certain';
+} else if (randomNumber = 1) {
+    eightBall = 'It is decidedly so';
+} else if (randomNumber = 2) {
+    eightBall = 'Reply hazy try again';
+} else if (randomNumber = 3) {
+    eightBall = 'Cannot predict now';
+} else if (randomNumber = 4) {
+    eightBall = 'Do not count on it';
+} else if (randomNumber == 5) {
+    eightBall = 'My sources say no';
+} else if (randomNumber == 6) {
+    eightBall = 'Outlook not so good';
+} else if (randomNumber = 7) {
+    eightBall = 'Signs point to yes';
+} else {
+    console.log('Error in code idiot');
+}
+
+console.log(randomNumber);
+
+console.log(eightBall);
